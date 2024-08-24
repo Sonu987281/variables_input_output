0e2a54104232205274daa59f8e0f5ed7d2fec567
Sonu987281 <saannukumarsingh@gmail.com>
2024-08-24 22:39:45 +0530

    Add files via upload

---
+++ 16_parctice.c
@@ -0,0 +1,107 @@
+wap to print add of three number 
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf(" enter first  number ");
+    scanf("%d",&a);
+    printf(" enter second number :");
+    scanf(" %d",&b);
+    printf(" entr third number");
+    scanf(" %d",&c);
+    printf(" %d\n",a+b+c);
+    return 0;
+
+
+
+}
+
+
+
+// wap to print multiplication of two number 
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b,c;
+//     printf(" enetr first number:");
+//     scanf(" %d",&a);
+//     printf(" enter second number : ");
+//     scanf(" %d",&b);
+//     printf(" enter third number: ");
+//     scanf(" %d",&c);
+
+//     printf(" %d\n",a*b*c);
+//     return 0;
+
+
+// }
+
+
+// wap to print area of square 
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int side;
+//     printf(" enetr an number:");
+//     scanf(" %d", &side);
+
+//     printf(" %d\n",side*side);
+//     return 0;
+// }
+
+
+
+// write a program   to print radius  
+
+// #include<stdio.h>
+// int main()
+// {
+//     float radius;
+//     printf(" enter an radius:");
+//     scanf(" %f",&radius);
+
+//     printf(" %f\n",3.14*radius*radius);
+//     return 0;
+
+// }
+
+
+//write a program to print substraction of three  number 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b,c;
+//     printf(" enter first number:");
+//     scanf(" %d",&a);
+//     printf(" enter second numbewr :");
+//     scanf(" %d",&b);
+//     printf(" enetr third number ");
+//     scanf(" %d",&c);
+
+//     printf(" %d\n",a-b+c);
+//     return 0;
+// }
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf(" enter first  number ");
+    scanf("%d",&a);
+    printf(" enter second number :");
+    scanf(" %d",&b);
+    printf(" entr third number");
+    scanf(" %d",&c);
+    printf(" %d\n",a+b+c);
+    return 0;
+
+
+
+}
